ece5a1749b4508b373582809913b1a231f5d5843
severinocarlos <juninho_456@hotmail.com>
2022-08-27 15:42:27 -0300

    add povoamento categoria_loja

--- Entrega 1/Povoamento/Povoamento Individual/povoamento_categoria_loja.sql
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_categoria_loja.sql
@@ -1,1 +1,10 @@
-INSERT INTO categoria_loja()
+-- povoamento categoria_loja
+
+INSERT INTO categoria_loja(categoria, loja) VALUES ('Vestuário', '44397911912301');
+INSERT INTO categoria_loja(categoria, loja) VALUES ('Eletrônicos', '95495393205116');
+INSERT INTO categoria_loja(categoria, loja) VALUES ('Automotivo', '90799725835943');
+INSERT INTO categoria_loja(categoria, loja) VALUES ('Livros', '76286940702533');
+INSERT INTO categoria_loja(categoria, loja) VALUES ('Cosmésticos', '86281239844091');
+INSERT INTO categoria_loja(categoria, loja) VALUES ('Esporte', '44035551046395');
+
+
