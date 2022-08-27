43bd08c3519c30d280487f066ef2b5b4d147ec5f
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 14:20:20 -0300

    Update produto.sql

--- Entrega 1/Tabelas/Tabelas Individuais/produto.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/produto.sql
@@ -1,8 +1,10 @@
 CREATE TABLE Produto(
+    identificacao VARCHAR2(255) NOT NULL,
+    cnpj_loja VARCHAR2(14) NOT NULL,
     nome VARCHAR2(255) NOT NULL,
     estoque NUMBER NOT NULL,
-    preço NUMBER NOT NULL,
-    média NUMBER NOT NULL,
-    CONSTRAINT produto_pk PRIMARY KEY (identificação, cnpj_loja), 
+    preco NUMBER NOT NULL,
+    media NUMBER NOT NULL,
+    CONSTRAINT produto_pk PRIMARY KEY (identificacao, cnpj_loja), 
     CONSTRAINT produto_fk FOREIGN KEY (cnpj_loja) REFERENCES Loja(cnpj_loja)
 );
