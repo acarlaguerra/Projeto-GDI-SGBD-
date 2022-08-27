b1318faa5c5fd924f75acbc72ed0df6ee6dc3455
severinocarlos <juninho_456@hotmail.com>
2022-08-27 12:17:16 -0300

    add categoria_loja

--- Entrega 1/Tabelas/Tabelas Individuais/categoria_loja.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/categoria_loja.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Categorias_loja( -- herda de loja
-    categoria VARCHAR2(10) NOT NULL,
+    categoria VARCHAR2(255) NOT NULL,
     loja NUMBER (14) NOT NULL,
     CONSTRAINT categorias_loja_pk PRIMARY KEY (loja, categoria),
     CONSTRAINT categorias_loja_fk FOREIGN KEY (loja) REFERENCES Loja(cnpj_loja)         
