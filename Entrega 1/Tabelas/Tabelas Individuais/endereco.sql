9c32be842ba4f38e8e35a718df02729997f51af2
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 14:06:43 -0300

    Update endereco.sql

--- Entrega 1/Tabelas/Tabelas Individuais/endereco.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/endereco.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Endereco (
-    cep VARCHAR2(255) NOT NULL,
+    cep VARCHAR2(8) NOT NULL,
     numero NUMBER NOT NULL,
     rua VARCHAR2(255) NOT NULL,
     
-    CONSTRAINT endereco_pk PRIMARY KEY (cep, numero),
+    CONSTRAINT endereco_pk PRIMARY KEY (cep, numero)
 );
