5b1d39c0f6d5861a260318626fbcde2004cdf4b9
Ana Carla <86753278+acarlaguerra@users.noreply.github.com>
2022-08-27 13:45:02 -0300

    Update avalia.sql

--- Entrega 1/Tabelas/Tabelas Individuais/avalia.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/avalia.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Avalia( -- relacionamento N:M
     cliente_cpf VARCHAR2(11) NOT NULL,
-    produto_id VARCHAR2(255) NOT NULL, 
+    produto_id INTEGER NOT NULL, 
     loja_cnpj VARCHAR2(14) NOT NULL, 
-    nota VARCHAR2(255) NOT NULL,
+    nota NUMBER CHECK (nota >= 1 AND nota <= 5),
     
     CONSTRAINT avalia_pk PRIMARY KEY (cliente_cpf, produto_id, loja_cnpj), 
     CONSTRAINT avalia_fk1 FOREIGN KEY (cliente_cpf) REFERENCES Cliente(cpf),
     CONSTRAINT avalia_fk2 FOREIGN KEY (produto_id) REFERENCES Produto(identificacao),
-    CONSTRAINT avalia_fk3 FOREIGN KEY (loja_cnpj) REFERENCES Produto(cnpj_loja),
+    CONSTRAINT avalia_fk3 FOREIGN KEY (loja_cnpj) REFERENCES Produto(cnpj_loja)
 );
