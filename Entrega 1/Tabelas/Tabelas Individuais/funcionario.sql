d1c218dc841fa561a1c15ab30f7da6404f181fb3
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 14:10:08 -0300

    Update funcionario.sql

--- Entrega 1/Tabelas/Tabelas Individuais/funcionario.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/funcionario.sql
@@ -3,7 +3,7 @@ CREATE TABLE Funcionario(
     data_de_admissao DATE NOT NULL,
     cpf_supervisor VARCHAR2(11) NOT NULL,
     
-    CONSTRAINT funcionario_pk PRIMARY KEY (cpf),
-    CONSTRAINT funcionario_fk FOREIGN KEY (cpf) REFERENCES Usuario(cpf),
-    CONSTRAINT funcionario_fk1 FOREIGN KEY (cpf_supervisor) REFERENCES Usuario(cpf)
+    CONSTRAINT funcionario_pk PRIMARY KEY (cpf_func),
+    CONSTRAINT funcionario_fk FOREIGN KEY (cpf_func) REFERENCES Usuario(cpf),
+    CONSTRAINT funcionario_fk1 FOREIGN KEY (cpf_supervisor) REFERENCES Funcionario(cpf_func)
 );
