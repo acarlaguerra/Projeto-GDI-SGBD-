aa1607cb2d0f1769ebe0b9e6b505409a80929dcd
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 15:09:31 -0300

    Create povoamento_cliente.sql

---
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_cliente.sql
@@ -0,0 +1,10 @@
+-- Povoamento: Cliente
+
+INSERT INTO Cliente(cpf_cliente, tipo_de_assinatura) VALUES ('12345678910', 'premium');
+INSERT INTO Cliente(cpf_cliente, tipo_de_assinatura) VALUES ('42172124417', 'gratuita');
+INSERT INTO Cliente(cpf_cliente, tipo_de_assinatura) VALUES ('52942165459', 'gratuita');
+INSERT INTO Cliente(cpf_cliente, tipo_de_assinatura) VALUES ('27945458922', 'premium');
+INSERT INTO Cliente(cpf_cliente, tipo_de_assinatura) VALUES ('95942585418', 'premium');
+INSERT INTO Cliente(cpf_cliente, tipo_de_assinatura) VALUES ('45492491529', 'gratuita');
+INSERT INTO Cliente(cpf_cliente, tipo_de_assinatura) VALUES ('57945725672', 'gratuita');
+INSERT INTO Cliente(cpf_cliente, tipo_de_assinatura) VALUES ('24924924928', 'premium');
