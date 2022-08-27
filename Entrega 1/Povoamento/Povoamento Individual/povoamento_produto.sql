129f8ea4915ccfe644ecb700bc4a3bc7162ca5a3
VS-Lucas <ufpelucasv@gmail.com>
2022-08-27 17:36:36 -0300

    produto precisa ajeitar a media

---
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_produto.sql
@@ -0,0 +1,18 @@
+--Povoamento produto
+
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '44035551046395', 'Matemática Discreta para Prodígios', '40', '150.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '44035551046395', 'Trivialidade Matemática', '100', '125.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '44397911912301', 'Blusa Relacional', '30', '70.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '44397911912301', 'Calça Jeans Conceitual', '25', '90.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '93139254465827', 'Os 3 Fundamentos de um Hacker de Sucesso', '10', '150.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '93139254465827', 'Hackeando um Sistema Passo a Passo', '5', '160.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '95495393205116', 'Par de Meias Shortest Path', '20', '30.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '95495393205116', 'Dijkstra Tênis', '15', '150.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '57503186114611', 'Óleo Lubrificante Superpipeline', '35', '100.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '57503186114611', 'Ar-condicionado Superescalar', '20', '500.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '90799725835943', 'CR7 Desodorante', '40', '45.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '90799725835943', 'Vasco Desodorante: antiqueda capilar', '30', '35.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '75167938739208', 'Hinode Lattitude', '35', '60.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '75167938739208', 'Traduções Gold', '20', '70.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '47087581321690', 'FutBô', '10', '450.00', );
+INSERT INTO Produto (identificacao, cnpj_loja, nome, estoque, preco, media) values (identificacao.NEXTVAL, '47087581321690', 'IA Raquete', '5', '550.00', );
