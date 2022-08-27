ab1ed0e0009a36b529eefc878c9d15ec6e75ef90
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 16:08:07 -0300

    Create povoamento_empresa.sql

---
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_empresa.sql
@@ -0,0 +1,12 @@
+-- Povoamento: Empresa
+
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('44035551046395', 'Livraria do Nivan');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('44397911912301', 'Valéria Modas');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('93139254465827', 'Hackeando o Siga');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('95495393205116', 'Gustavo Corridas');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('57503186114611', 'Adriano Motors');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('90799725835943', 'Jequiti');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('75167938739208', 'Hinode');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('47087581321690', 'CIn Sports');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('86281239844091', 'Felipe Quebra Códigos');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('76286940702533', 'Stewart & Friends');
