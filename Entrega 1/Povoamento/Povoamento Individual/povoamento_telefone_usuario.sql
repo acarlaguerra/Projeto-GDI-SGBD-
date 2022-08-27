a9d544826144fad83960b37243c6247691121dff
severinocarlos <juninho_456@hotmail.com>
2022-08-27 16:29:39 -0300

    add povoamento telefone_user

---
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_telefone_usuario.sql
@@ -0,0 +1,13 @@
+-- povoamento Telefone_usuario (esperar fazer user)
+
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('12345678910', '998804842')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('42172124417', '991861127')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('52942165459', '993968102')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('27945458922', '995840355')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('95942585418', '994059907')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('45492491529', '998170764')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('57945725672', '987769452')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('24924924928', '997637451')
+-- adicionar novos cpf's
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('12345678910', '981375453')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('12345678910', '998800617')
