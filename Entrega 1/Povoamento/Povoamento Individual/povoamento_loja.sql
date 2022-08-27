417c103528271b101abeeb3622e5fbc743a3687c
Lucas Vinícius <83307085+VS-Lucas@users.noreply.github.com>
2022-08-27 15:34:39 -0300

    Create povoamento_loja.sql

---
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_loja.sql
@@ -0,0 +1,12 @@
+--Povoamento loja
+
+INSERT INTO Loja (cnpj_loja) values ('44035551046395');
+INSERT INTO Loja (cnpj_loja) values ('44397911912301');
+INSERT INTO Loja (cnpj_loja) values ('93139254465827');
+INSERT INTO Loja (cnpj_loja) values ('95495393205116');
+INSERT INTO Loja (cnpj_loja) values ('57503186114611');
+INSERT INTO Loja (cnpj_loja) values ('90799725835943');
+INSERT INTO Loja (cnpj_loja) values ('75167938739208');
+INSERT INTO Loja (cnpj_loja) values ('47087581321690');
+INSERT INTO Loja (cnpj_loja) values ('86281239844091');
+INSERT INTO Loja (cnpj_loja) values ('76286940702533');
