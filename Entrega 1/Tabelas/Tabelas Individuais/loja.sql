e512e3bb10fb6c09cccf2c6bcddd83652860d8af
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 14:14:22 -0300

    Update loja.sql

--- Entrega 1/Tabelas/Tabelas Individuais/loja.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/loja.sql
@@ -1,4 +1,5 @@
-CREATE TABLE Loja(
-    CONSTRAINT loja_pk PRIMARY KEY (cnpj_loja)
+CREATE TABLE Loja( -- Herda de empresa
+    cnpj_loja VARCHAR2(14) NOT NULL,
+    CONSTRAINT loja_pk PRIMARY KEY (cnpj_loja),
     CONSTRAINT loja_fk FOREIGN KEY (cnpj_loja) REFERENCES Empresa (cnpj)
 );
