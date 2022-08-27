aca2fcb038aa3953615dc44198a4184457ad0bc9
Lucas Vinícius <83307085+VS-Lucas@users.noreply.github.com>
2022-08-27 15:31:02 -0300

    Update povoamento_transportadora.sql

--- Entrega 1/Povoamento/Povoamento Individual/povoamento_transportadora.sql
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_transportadora.sql
@@ -1,12 +1,12 @@
 --Povoamento transportadora
 
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('44035551046395', '65')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('44397911912301', '60')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('93139254465827', '70')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('95495393205116', '65')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('57503186114611', '55')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('90799725835943', '75')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('75167938739208', '60')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('47087581321690', '70')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('86281239844091', '80')
-INSERT INTO Transportadora (cnpj_transportadora, frete) values ('76286940702533', '75')
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('44035551046395', '65');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('44397911912301', '60');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('93139254465827', '70');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('95495393205116', '65');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('57503186114611', '55');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('90799725835943', '75');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('75167938739208', '60');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('47087581321690', '70');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('86281239844091', '80');
+INSERT INTO Transportadora (cnpj_transportadora, frete) values ('76286940702533', '75');
