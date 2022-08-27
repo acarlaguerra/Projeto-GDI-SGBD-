b59c66fc50a37a4c4f7ab300736a58239371682d
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 14:36:25 -0300

    Update reclama.sql

--- Entrega 1/Tabelas/Tabelas Individuais/reclama.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/reclama.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Reclama( -- relacionamento triplo
     cliente_cpf VARCHAR2(11) NOT NULL,
-    pedido_id VARCHAR2(255) NOT NULL,
+    pedido_id INTEGER NOT NULL,
     funcionario_cpf VARCHAR2(11) NOT NULL,
     classificacao VARCHAR2(255) NOT NULL,
     descricao VARCHAR2(255) NOT NULL,
     
     CONSTRAINT reclama_pk PRIMARY KEY (cliente_cpf, pedido_id, funcionario_cpf), 
-    CONSTRAINT reclama_fk1 FOREIGN KEY (cliente_cpf) REFERENCES Cliente(cpf),
+    CONSTRAINT reclama_fk1 FOREIGN KEY (cliente_cpf) REFERENCES Cliente(cpf_cliente),
     CONSTRAINT reclama_fk2 FOREIGN KEY (pedido_id) REFERENCES Pedido(ID_do_pedido),
-    CONSTRAINT reclama_fk3 FOREIGN KEY (funcionario_cpf) REFERENCES Funcionario(cpf)
+    CONSTRAINT reclama_fk3 FOREIGN KEY (funcionario_cpf) REFERENCES Funcionario(cpf_func)
 );
