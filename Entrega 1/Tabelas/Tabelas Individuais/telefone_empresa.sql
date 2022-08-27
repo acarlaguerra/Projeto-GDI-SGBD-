8ce5aacfdf749e27236c3585412418e8af858a41
severinocarlos <juninho_456@hotmail.com>
2022-08-27 16:01:08 -0300

    ajuste

--- Entrega 1/Tabelas/Tabelas Individuais/telefone_empresa.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/telefone_empresa.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Telefone_empresa( -- herda de empresa
     empresa NUMBER(14) NOT NULL,
-    fone NUMBER(9) NOT NULL,
+    fone NUMBER(11) NOT NULL,
     CONSTRAINT telefone_empresa_pk PRIMARY KEY (empresa, fone),
     CONSTRAINT telefone_empresa_fk FOREIGN KEY (empresa) REFERENCES Empresa(cnpj)
 );
