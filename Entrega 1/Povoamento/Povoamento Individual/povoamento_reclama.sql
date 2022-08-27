b6102353d59ce3cefe99f44ae28cb1e005b41bcb
severinocarlos <juninho_456@hotmail.com>
2022-08-27 17:05:43 -0300

    comments

--- Entrega 1/Povoamento/Povoamento Individual/povoamento_reclama.sql
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_reclama.sql
@@ -1,1 +1,4 @@
+-- povoando Reclama
+-- Povoar pedido primeiro antes de povoar reclama
+
 INSERT INTO Reclama(cliente_cpf, pedido_id, funcionario_cpf, classificacao, descricao) VALUES ('42172124417', '', '', '', '', '') 
--- Entrega 1/Povoamento/Povoamento Individual/povoamento_telefone_usuario.sql
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_telefone_usuario.sql
@@ -8,6 +8,6 @@ INSERT INTO Telefone_usuario(cpf, fone) VALUES ('95942585418', '994059907')
 INSERT INTO Telefone_usuario(cpf, fone) VALUES ('45492491529', '998170764')
 INSERT INTO Telefone_usuario(cpf, fone) VALUES ('57945725672', '987769452')
 INSERT INTO Telefone_usuario(cpf, fone) VALUES ('24924924928', '997637451')
--- adicionar novos cpf's
+-- adicionar novos cpf's que forem adicionados em usuários
 INSERT INTO Telefone_usuario(cpf, fone) VALUES ('12345678910', '981375453')
 INSERT INTO Telefone_usuario(cpf, fone) VALUES ('12345678910', '998800617')
