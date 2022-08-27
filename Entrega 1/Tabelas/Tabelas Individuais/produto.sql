385afe3436cd33cca98e76575fb6fa5fc0c080bd
joaomarcelovalencachacon <86393443+joaomarcelovalencachacon@users.noreply.github.com>
2022-08-27 12:40:50 -0300

    Update produto.sql

--- Entrega 1/Tabelas/Tabelas Individuais/produto.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/produto.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Produto(
-     VARCHAR2(255) NOT NULL,
     nome VARCHAR2(255) NOT NULL,
     estoque NUMBER NOT NULL,
     preço NUMBER NOT NULL,
+    média NUMBER NOT NULL,
     CONSTRAINT produto_pk PRIMARY KEY (identificação, cnpj_loja), 
     CONSTRAINT produto_fk FOREIGN KEY (cnpj_loja) REFERENCES Loja(cnpj_loja)
 );
