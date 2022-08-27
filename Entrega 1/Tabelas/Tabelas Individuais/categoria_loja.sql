1d5a56611b9f47f2a3ddf004ef7f00cc0ac722d0
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 14:02:03 -0300

    Update categoria_loja.sql

--- Entrega 1/Tabelas/Tabelas Individuais/categoria_loja.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/categoria_loja.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Categorias_loja( -- herda de loja
     categoria VARCHAR2(255) NOT NULL,
-    loja NUMBER (14) NOT NULL,
+    loja VARCHAR2(14) NOT NULL,
     CONSTRAINT categorias_loja_pk PRIMARY KEY (loja, categoria),
     CONSTRAINT categorias_loja_fk FOREIGN KEY (loja) REFERENCES Loja(cnpj_loja)         
 );
