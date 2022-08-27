33d841799cb41abb827544625111e2781f75e8eb
CarolinaBerrafato <85506019+CarolinaBerrafato@users.noreply.github.com>
2022-08-27 14:00:50 -0300

    Update cliente.sql

--- Entrega 1/Tabelas/Tabelas Individuais/cliente.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/cliente.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Cliente( -- herda de Usuario
-    cpf_cliente NUMBER(11),
-    tipo_de_assinatura VARCHAR2(255),
+    cpf_cliente NUMBER(11) NOT NULL,
+    tipo_de_assinatura VARCHAR2(255) NOT NULL,
     CONSTRAINT cliente_pk PRIMARY KEY (cpf_cliente),
     CONSTRAINT cliente_fk FOREIGN KEY (cpf_cliente) REFERENCES Usuario(cpf)
 );
