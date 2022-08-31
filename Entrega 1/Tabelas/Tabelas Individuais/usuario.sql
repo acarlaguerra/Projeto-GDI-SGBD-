ffb230de8fe88048f18f0b546e377e6cb06eaf3d
Ana Carla <86753278+acarlaguerra@users.noreply.github.com>
2022-08-31 16:13:57 -0300

    Update usuario.sql

--- Entrega 1/Tabelas/Tabelas Individuais/usuario.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/usuario.sql
@@ -2,7 +2,8 @@ CREATE TABLE Usuario (
     cpf VARCHAR2(11) NOT NULL, 
     nome VARCHAR2(255) NOT NULL,
     cep_endereco VARCHAR2(255) NOT NULL,
+    numero_endereco NUMBER NOT NULL
     
     CONSTRAINT pessoa_pk PRIMARY KEY (cpf),
-    CONSTRAINT pessoa_fk FOREIGN KEY (cep_endereco) REFERENCES Endereco(cep)
+    CONSTRAINT pessoa_fk FOREIGN KEY (cep_endereco, numero_endereco) REFERENCES Endereco(cep, numero)
 );
