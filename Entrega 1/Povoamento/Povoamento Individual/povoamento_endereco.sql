2e1ee0f3f694f8457ee17fb38dd089af3df6f38e
VS-Lucas <ufpelucasv@gmail.com>
2022-08-31 20:06:12 -0300

    endereço povoado

---
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_endereco.sql
@@ -0,0 +1,15 @@
+--Povoamento endereço
+
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000000', 20, 'Rua da Alegria');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000001', 54, 'Rua da Pedra');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000002', 87, 'Rua da Alegria');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000003', 475, 'Rua da Moeda');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000004', 123, 'Rua Augustinho Carrara');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000005', 65, 'Rua do Bonfim');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000006', 11, 'Rua da Moeda');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000007', 410, 'Rua Bom Jesus');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000008', 96, 'Rua das Graças');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000009', 20, 'Rua do Curado');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000010', 19, 'Rua da Penha');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000011', 44, 'Rua Aqui Acabou');
+INSERT INTO Endereco(cep, numero, rua) VALUES ('50000012', 32, 'Rua dos Desesperados');
