c37cacb467b0ddde5871f59760b21f88220f58de
Lucas Vinícius <83307085+VS-Lucas@users.noreply.github.com>
2022-08-30 22:24:20 -0300

    retira média

--- Entrega 1/Tabelas/Tabelas Individuais/produto.sql
+++ Entrega 1/Tabelas/Tabelas Individuais/produto.sql
@@ -4,7 +4,6 @@ CREATE TABLE Produto(
     nome VARCHAR2(255) NOT NULL,
     estoque NUMBER NOT NULL,
     preco NUMBER NOT NULL,
-    media NUMBER NOT NULL,
     CONSTRAINT produto_pk PRIMARY KEY (identificacao, cnpj_loja), 
     CONSTRAINT produto_fk FOREIGN KEY (cnpj_loja) REFERENCES Loja(cnpj_loja)
 );
