1d858c3ba5603ed5ecc6d63a371461502c4ca370
Ana Carla <86753278+acarlaguerra@users.noreply.github.com>
2022-08-31 16:40:05 -0300

    Create povoamento_usuario.sql

---
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_usuario.sql
@@ -0,0 +1,15 @@
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('12345678910', 'Silvinho Barros', '50000000', 20);    -- CLIENTE E FUNC
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('42172124417', 'Hanninha Nóbrega', '50000001', 54);    -- CLIENTE E FUNC
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('52942165459', 'Nivanzinho Ferreira', '50000002', 87);    -- CLIENTE E FUNC
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('27945458922', 'Marcelinho Barros', '50000003', 475);    -- CLIENTE
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('95942585418', 'Filipinho Calegário', '50000004', 123);    -- CLIENTE
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('45492491529', 'Ricardinho Prudêncio', '50000005', 65);    -- CLIENTE
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('57945725672', 'Biel Albuquerque', '50000006', 11);    -- CLIENTE 
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('24924924928', 'Aninha Albuquerque', '50000006', 11);    -- CLIENTE 
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('27433969307', 'Carlinhos Ferraz', '50000007', 410);    -- FUNCIONARIO
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('43413647537', 'Carolzinha Berrafato', '50000008', 96);    -- FUNCIONARIO
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('45026905315', 'Joãozinho Borsoi', '50000009', 20);    -- FUNCIONARIO
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('16922419254', 'Bonninha Borsoi', '50000009', 20);    -- FUNCIONARIO
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('68722270407', 'Bill Gates', '50000010', 19);    -- FUNCIONARIO
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('60692905833', 'Luquinhas Silva', '50000011', 44);    -- FUNCIONARIO
+INSERT INTO Usuario(cpf, nome, cep_endereco, numero_endereco) VALUES ('29526345298', 'Ricardinho Massa', '50000012', 32);    -- FUNCIONARIO
