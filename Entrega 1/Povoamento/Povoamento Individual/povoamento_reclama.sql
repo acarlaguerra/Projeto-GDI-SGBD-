fcdd84b1a89f9434855026bf7e56386ea6772ca0
Bonna Borsoi <86304672+bonnaborsoi@users.noreply.github.com>
2022-08-31 21:30:35 -0300

    Update povoamento_reclama.sql

--- Entrega 1/Povoamento/Povoamento Individual/povoamento_reclama.sql
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_reclama.sql
@@ -1,4 +1,8 @@
--- povoando Reclama
--- Povoar pedido primeiro antes de povoar reclama
+-- Povoamento Reclama
 
-INSERT INTO Reclama(cliente_cpf, pedido_id, funcionario_cpf, classificacao, descricao) VALUES ('42172124417', '', '', '', '', '') 
+INSERT INTO Reclama(cliente_cpf, pedido_id, funcionario_cpf, classificacao, descricao) VALUES ('42172124417', 2, '68722270407', 'entrega', 'o entregador jogou a encomenda pelo portão'); 
+INSERT INTO Reclama(cliente_cpf, pedido_id, funcionario_cpf, classificacao, descricao) VALUES ('529421654597', 6, '29526345298', 'produto', 'a garrafa veio de outra cor e quebrada'); 
+INSERT INTO Reclama(cliente_cpf, pedido_id, funcionario_cpf, classificacao, descricao) VALUES ('12345678910', 4, '60692905833', 'funcionario', 'funcionario foi rude e não me ajudou'); 
+INSERT INTO Reclama(cliente_cpf, pedido_id, funcionario_cpf, classificacao, descricao) VALUES ('27945458922', 1, '68722270407', 'entrega', 'a entrega atrasou mais de 2 semanas'); 
+INSERT INTO Reclama(cliente_cpf, pedido_id, funcionario_cpf, classificacao, descricao) VALUES ('57945725672', 7, '16922419254', 'produto', 'produto menor do que eu esperava'); 
+INSERT INTO Reclama(cliente_cpf, pedido_id, funcionario_cpf, classificacao, descricao) VALUES ('95942585418', 3, '29526345298', 'produto', 'livro chegou amassado');
