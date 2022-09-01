28247912eb5155d07d7b0864c6fac6564872e3db
Ana Carla <86753278+acarlaguerra@users.noreply.github.com>
2022-08-31 23:07:54 -0300

    Update povoamento_empresa.sql

--- Entrega 1/Povoamento/Povoamento Individual/povoamento_empresa.sql
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_empresa.sql
@@ -8,8 +8,12 @@ INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('57503186114611', 'Adriano Moto
 INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('90799725835943', 'Jequiti');
 INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('75167938739208', 'Hinode - Entregamos na sua casa');
 INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('47087581321690', 'CIn Sports - Temos delivery');
-INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('86281239844091', 'Caminho da UFPE Transporte e Ltda');
-INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('76286940702533', 'Tempo de prova: Chega Rápido');
+
+-- apenas lojas
 INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('75167938739001', 'Salgadinho do Marcelo');
 INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('47087581321002', 'Quentinhas de tio Almir');
 INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('75167938739003', 'Sacolé do CAC');
+
+--apenas transportadoras
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('86281239844091', 'Caminho da UFPE Transporte e Ltda');
+INSERT INTO Empresa(cnpj, nome_fantasia) VALUES ('76286940702533', 'Tempo de prova: Chega Rápido');
