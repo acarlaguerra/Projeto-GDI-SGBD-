04d20a050892f099234f2e9c8494732a2d40128a
severinocarlos <juninho_456@hotmail.com>
2022-08-31 21:55:03 -0300

    ajustes

--- Entrega 1/Povoamento/Povoamento Individual/povoamento_telefone_usuario.sql
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_telefone_usuario.sql
@@ -1,18 +1,18 @@
 -- povoamento Telefone_usuario
 
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('12345678910', '998804842') 
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('42172124417', '991861127')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('52942165459', '993968102')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('27945458922', '995840355')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('95942585418', '994059907')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('45492491529', '998170764')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('57945725672', '987769452')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('24924924928', '997637451')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('12345678910', '998804842'); 
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('42172124417', '991861127');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('52942165459', '993968102');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('27945458922', '995840355');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('95942585418', '994059907');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('45492491529', '998170764');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('57945725672', '987769452');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('24924924928', '997637451');
 -- adicionar novos cpf's que forem adicionados em usuários ok
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('27433969307', '981375453')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('43413647537', '998800617')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('45026905315', '998810617')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('16922419254', '998820617')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('68722270407', '998830617')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('60692905833', '998840617')
-INSERT INTO Telefone_usuario(cpf, fone) VALUES ('29526345298', '998850617')
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('27433969307', '981375453');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('43413647537', '998800617');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('45026905315', '998810617');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('16922419254', '998820617');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('68722270407', '998830617');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('60692905833', '998840617');
+INSERT INTO Telefone_usuario(cpf, fone) VALUES ('29526345298', '998850617');
