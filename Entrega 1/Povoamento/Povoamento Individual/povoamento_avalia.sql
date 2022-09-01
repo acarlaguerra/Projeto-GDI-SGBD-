2e4bcd40ec8e42738bfad02da031fab0d4686e83
VS-Lucas <ufpelucasv@gmail.com>
2022-08-31 21:12:09 -0300

    avalia povoado

---
+++ Entrega 1/Povoamento/Povoamento Individual/povoamento_avalia.sql
@@ -0,0 +1,8 @@
+-- Povoamento avalia
+
+INSERT INTO Avalia(cliente_cpf, nome_produto, loja_cnpj, nota) VALUES ('42172124417', 'Matemática Discreta para Prodígios', '44035551046395', 1);
+INSERT INTO Avalia(cliente_cpf, nome_produto, loja_cnpj, nota) VALUES ('52942165459', 'Vasco Desodorante: antiqueda capilar', '90799725835943', 5);
+INSERT INTO Avalia(cliente_cpf, nome_produto, loja_cnpj, nota) VALUES ('24924924928', 'Blusa Relacional', '44397911912301', 5);
+INSERT INTO Avalia(cliente_cpf, nome_produto, loja_cnpj, nota) VALUES ('57945725672', 'Óleo Lubrificante Superpipeline', '57503186114611', 3);
+INSERT INTO Avalia(cliente_cpf, nome_produto, loja_cnpj, nota) VALUES ('45492491529', 'Dijkstra Tênis', '95495393205116', 5);
+INSERT INTO Avalia(cliente_cpf, nome_produto, loja_cnpj, nota) VALUES ('95942585418', 'Os 3 Fundamentos de um Hacker de Sucesso', '93139254465827', 4);
